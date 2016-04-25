5227554e78305eee25dcdaf82680fc418857aeac
Enzo <e.amespizarro@gmail.com>
2016-04-25 15:20:54 -0400

    max_flow work

--- max_flow.c
+++ max_flow.c
@@ -24,12 +24,68 @@ path from s to t with positive residual capacity.
 You have to allocate the auxiliary arrays. You can use either BFS or DFS for the search of the augmenting path.
 */
 
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
-void maximum_flow(int n, int s, int t, int *capacity, int *flow);
+void maximum_flow(int n, int s, int t, int capacity[][n], int flow[][n] )
+{
+    int residualGraph[n][n];
+    int i, j;
+
+    //Copy capacity graph to residual graph
+    printf("\nPrinting Capacity on function max_flow \n\n");
+
+    for (i = 0 ;  i < n ; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            residualGraph[i][j] = capacity[i][j];
+            printf("%6i", residualGraph[i][j] );
+        }
+        printf("\n");
+    }
+}
 
 
 int main()
 {
+    int vertices = 6; int source = 0; int target = 5; //target = n - 1
+
+    //int capacityMatrx = [][]
+
+    int capacityMatrx [6][6];
+    int flowMatrx [6][6];
+    int i, j;
+    printf("Enter Capacity for matrix: \n");
+
+    for (i = 0 ; i < vertices ; i++ ) //rows
+    {
+        for (j = 0 ; j < vertices ; j++) //columns
+        {
+            scanf("%d", &capacityMatrx[i][j]);
+        }
+    }
+
+    for (i = 0 ; i < vertices ; i++ ) //rows
+    {
+        for (j = 0 ; j < vertices ; j++) //columns
+        {
+            flowMatrx[i][j] = 0;
+        }
+    }
+
+//    //Print capacity Matrix
+//    for (i = 0 ; i < vertices ; i++ )
+//    {
+//        for (j = 0 ; j < vertices ; j++)
+//        {
+//            printf("%6i", capacityMatrx[i][j]);
+//        }
+//        printf("\n");
+//    }
+
+
+    maximum_flow(vertices, source, target, capacityMatrx, flowMatrx );
+
     return 0;
 }
