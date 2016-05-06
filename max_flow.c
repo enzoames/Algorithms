105047c1692a56d2adae98754b79a2c5847715a3
Enzo <e.amespizarro@gmail.com>
2016-05-05 22:44:02 -0400

    work on max flow function

--- max_flow.c
+++ max_flow.c
@@ -26,32 +26,124 @@ You have to allocate the auxiliary arrays. You can use either BFS or DFS for the
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-void maximum_flow(int n, int s, int t, int capacity[][n], int flow[][n] )
+bool bfs(int n, int start, int end, int * residualG, int path[]);
+void maximum_flow(int n, int s, int t, int *capacityMtrx, int *flowMtrx );
+void addToQueue(int x);
+int removeFromQueue();
+
+//Global Variables
+int head = 0; int tail = 0;
+int queue[10000];
+
+
+
+void maximum_flow(int n, int s, int t, int *capacityMtrx, int *flowMtrx )
 {
-    int residualGraph[n][n];
+    int residualGraph[n][n]; //to update the capacities as we go
+    int path[n];
     int i, j;
+    int u, v;
 
-    //Copy capacity graph to residual graph
-    printf("\nPrinting Capacity on function max_flow \n\n");
-
-    for (i = 0 ;  i < n ; i++)
+    //Residual_Capacity = Capacity
+    //Initialyze Flow Matrix to zero
+    for (i = 0 ; i < n ; i++ ) //rows
     {
-        for (j = 0; j < n; j++)
+        for (j = 0 ; j < n ; j++) //columns
         {
-            residualGraph[i][j] = capacity[i][j];
-            printf("%6i", residualGraph[i][j] );
+            residualGraph[i][j] = *(capacityMtrx + i*n + j);
+            *(flowMtrx + i*n + j) = 0;
         }
-        printf("\n");
     }
+
+    //Agument the path from s to t
+    while(bfs(n, s, t, &(residualGraph[0][0]), path)
+    {
+
+
+
+    }
+
+
+
+
 }
+
+
+//    int temp = 0;
+//    while (!temp)
+//    {
+//        u = 0;
+//        for (v = 0; v < n ; v++)
+//        {
+//            if( residualGraph[u][v] - flowMtrx[u][v] > 0 ) //if greater than 0 then there is a link.
+//            {
+//                addToQueue(v);
+//            }
+//        }
+//
+//
+//
+//    }
+//
+//  BFS(n,s,t);
+
+
+bool bfs(int n, int start, int end, int *residualG, int path[])// n = number of vertices , start = 0 , end = 5
+{
+    int visitedVertex[n];
+    int t, u;
+
+    for (u = 0 ; u < n ; u++) //initialyze the visited vertices to zero
+        visitedVertex[u] = 0; //false
+
+    addToQueue(start);
+    visitedVertex[start] = true;
+    path[start] = -1;
+
+    while(head!=tail)
+    {
+        t = removeFromQueue();
+
+        for (u = 0; u < n ; u++)
+        {
+            if (*(residualG + t*n + u) > 0 && visitedVertex[u] == false )
+            {
+                addToQueue(u);
+                visitedVertex[u] = true;
+                path[u] = t;
+            }
+        }
+    }
+
+    if (visitedVertex[end] == true)
+        return true;
+    else
+        return false;
+
+}
+
+void addToQueue(int x)
+{
+    queue[tail] = x;
+    tail++;
+
+}
+
+int removeFromQueue()
+{
+    int s;
+    s = queue[head];
+    head ++;
+    return s;
+}
+
 
 
 int main()
 {
     int vertices = 6; int source = 0; int target = 5; //target = n - 1
-
-    //int capacityMatrx = [][]
 
     int capacityMatrx [6][6];
     int flowMatrx [6][6];
@@ -66,26 +158,8 @@ int main()
         }
     }
 
-    for (i = 0 ; i < vertices ; i++ ) //rows
-    {
-        for (j = 0 ; j < vertices ; j++) //columns
-        {
-            flowMatrx[i][j] = 0;
-        }
-    }
 
-//    //Print capacity Matrix
-//    for (i = 0 ; i < vertices ; i++ )
-//    {
-//        for (j = 0 ; j < vertices ; j++)
-//        {
-//            printf("%6i", capacityMatrx[i][j]);
-//        }
-//        printf("\n");
-//    }
-
-
-    maximum_flow(vertices, source, target, capacityMatrx, flowMatrx );
+    maximum_flow(vertices, source, target, &(capacityMatrx[0][0]), &(flowMatrx[0][0]) );
 
     return 0;
 }
